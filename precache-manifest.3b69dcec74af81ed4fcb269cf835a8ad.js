c7f4a8c7d805fe10899ea0dfba4f9a23d136df9d
porosh2002 <jamilkashem2002@gmail.com>
2020-08-19 01:53:58 +0600

    Updates

---
+++ precache-manifest.3b69dcec74af81ed4fcb269cf835a8ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f03ba4334cf4e85f301dd2265ee221",
+    "url": "/3pshopping/index.html"
+  },
+  {
+    "revision": "947f45914573dead81ed",
+    "url": "/3pshopping/static/css/2.1e97e3df.chunk.css"
+  },
+  {
+    "revision": "4798314bc883c517690a",
+    "url": "/3pshopping/static/css/3.e21cb845.chunk.css"
+  },
+  {
+    "revision": "06b7d2b379918bb8db8c",
+    "url": "/3pshopping/static/css/4.75bc6138.chunk.css"
+  },
+  {
+    "revision": "cca03135c4c54465cfed",
+    "url": "/3pshopping/static/css/main.7bf72464.chunk.css"
+  },
+  {
+    "revision": "947f45914573dead81ed",
+    "url": "/3pshopping/static/js/2.c57f4cdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/3pshopping/static/js/2.c57f4cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4798314bc883c517690a",
+    "url": "/3pshopping/static/js/3.5ceec7e5.chunk.js"
+  },
+  {
+    "revision": "06b7d2b379918bb8db8c",
+    "url": "/3pshopping/static/js/4.11577322.chunk.js"
+  },
+  {
+    "revision": "cca03135c4c54465cfed",
+    "url": "/3pshopping/static/js/main.2eb338bd.chunk.js"
+  },
+  {
+    "revision": "37d7b1dc1502fcfc75a5",
+    "url": "/3pshopping/static/js/runtime-main.63aa95ad.js"
+  },
+  {
+    "revision": "4e73d0b1056588b16dde5e1fc3d36066",
+    "url": "/3pshopping/static/media/logo.4e73d0b1.jpg"
+  }
+]);
